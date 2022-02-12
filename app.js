030c3533dd5b9c8e6a01ecbe38d65a6da83fdff4
Krystian S <mr.krystian.stawarczyk@gmail.com>
2022-02-12 19:03:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+//Game Values //
+
+let min = 1,
+	max = 10,
+	winningNumb = 2,
+	guessesLeft = 3;
+
+// UI //
+
+const game = document.querySelector("#game"),
+	minNum = document.querySelector(".min-num"),
+	maxNum = document.querySelector(".max-num"),
+	guessBtn = document.querySelector("#guess-btn"),
+	guessInput = document.querySelector("#guess-input"),
+	message = document.querySelector(".message");
+
+// Assign min and max values UI //
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listening btn for guess//
+
+guessBtn.addEventListener("click", function () {
+	let guess = parseInt(guessInput.value);
+	//Validation//
+	if (isNaN(guess) || guess < min || guess > max) {
+		setMessage(`Please enter proper number between ${min} and ${max}`, "red");
+	}
+});
+
+// Set message
+function setMessage(msg, color) {
+	message.textContent = msg;
+	message.style.color = color;
+}
